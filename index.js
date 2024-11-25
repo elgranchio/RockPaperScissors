66ce0a33a7db336576536bc8a141ad4fa973fb36
elgranchio <184424724+elgranchio@users.noreply.github.com>
2024-11-25 19:59:10 +0100

    created logic for one round

--- index.js
+++ index.js
@@ -25,7 +25,19 @@ let computerScore = 0;
 
 function playRound( humanChoice, computerChoice ) {
     let result;
-
+    if (humanChoice === computerChoice) {
+        console.log(`It's a Tie!, both players chose ${humanChoice}`);
+    }
+else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+ (humanChoice === "scissors" && computerChoice ==="paper") ||
+  (humanChoice === "paper" && computerChoice === "rock")) {
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++
+  }
+  else {
+    console.log( `You Loose! ${computerChoice} beats ${humanChoice}`)
+    computerScore++
+  }
 
 }
 
